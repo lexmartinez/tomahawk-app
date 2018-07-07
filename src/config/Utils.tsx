2fdcc7979286423b305ff549b539c4c2dec51e43
Lex Martinez <lex0316@gmail.com>
2018-07-07 13:50:29 -0500

    adding responsive h/w support by OS

--- src/config/Utils.tsx
+++ src/config/Utils.tsx
@@ -1,5 +1,10 @@
 import { widthPercentageToDP as wdp, heightPercentageToDP as hdp} from 'react-native-responsive-screen'
 import { isIphoneX } from 'react-native-iphone-x-helper'
+import { Platform } from 'react-native'
+
+export function isIOS() {
+    return Platform.OS === 'ios'
+}
 
 export function hp(val: string, iXVal?: string) {
    return isIphoneX() ? hdp(iXVal ? iXVal : val) : hdp(val)
@@ -7,4 +12,20 @@ export function hp(val: string, iXVal?: string) {
 
 export function wp(val: string, iXVal?: string) {
     return isIphoneX() ? wdp(iXVal ? iXVal : val) : wdp(val)
+}
+
+export function hpOS(android: string, ios: string, iphoneX?: string) {
+    if (isIOS()) {
+        return isIphoneX() ? hdp(iphoneX ? iphoneX : ios) : hdp(ios)
+    } else {
+        return hdp(android)
+    }
+}
+
+export function wpOS(android: string, ios: string, iphoneX?: string) {
+    if (isIOS()) {
+        return isIphoneX() ? wdp(iphoneX ? iphoneX : ios) : wdp(ios)
+    } else {
+        return wdp(android)
+    }
 }
