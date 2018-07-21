e4008797f5ea4ee6b870aa61368f6060ebd19490
Lex Martinez <lex0316@gmail.com>
2018-07-21 14:44:57 -0500

    removing deprecated igdb api key

--- src/config/Constants.tsx
+++ src/config/Constants.tsx
@@ -45,4 +45,4 @@ export const AppInfo = {
 export const NEWS_PLACEHOLDER = 'https://www.rbs.ca/wp-content/themes/rbs/images/news-placeholder.png'
 export const API_URL = 'https://api-endpoint.igdb.com'
 export const INFO_URL = 'https://api.github.com/users/lexmartinez'
-export const API_KEY = {'user-key' : '7b39bcdaf866ddd2b2e1f398e4efe8d1'}
+export const API_KEY = {'user-key' : 'IGDB-API-KEY'}
